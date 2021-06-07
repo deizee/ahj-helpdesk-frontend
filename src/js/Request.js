2ff01b90fc685c9a903cd4d925b73f48f68a90ab
deizee <deizee@mail.ru>
2021-06-07 11:21:21 +0500

    changed baseUrl in Request.js

--- src/js/Request.js
+++ src/js/Request.js
@@ -1,6 +1,5 @@
 export default async function createRequest(options) {
-  // const baseUrl = 'https://ahj-helpdesk-backend.herokuapp.com/';
-  const baseUrl = 'http://localhost:3000/';
+  const baseUrl = 'https://ahj-helpdesk-backend.herokuapp.com/';
   const requestUrl = baseUrl + options.url;
   const response = await fetch(requestUrl, {
     method: options.method || 'GET',
